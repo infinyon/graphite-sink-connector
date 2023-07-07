0394228ce107a00e120e0ef7e92eea3f1843c935
Esteban Borai <estebanborai@gmail.com>
2023-07-07 13:43:06 -0400

    feat: tcp streaming for plain text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod config;
 mod graphite;
 
+use std::io::Write;
+use std::net::TcpStream;
+
 use fluvio_connector_common::consumer::ConsumerStream;
 use fluvio_connector_common::{connector, Result};
 
@@ -11,10 +14,14 @@ use self::graphite::GraphiteMessage;
 async fn start(config: CustomConfig, mut stream: impl ConsumerStream) -> Result<()> {
     println!("Starting graphite-connector sink connector with {config:?}");
 
+    let mut graphite_tcp = TcpStream::connect("127.0.0.1:2003")?;
+
     while let Some(Ok(record)) = stream.next().await {
         let value_bytes = record.value();
         let value_utf8 = String::from_utf8_lossy(value_bytes).to_string();
         let message = GraphiteMessage::new(config.metric_path.clone(), value_utf8);
+
+        graphite_tcp.write(message.to_string().as_bytes())?;
     }
 
     Ok(())
